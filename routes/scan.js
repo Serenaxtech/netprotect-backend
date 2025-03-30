ffe5b7bdd36502e525e8a95de9d05e162d14702d
serena <serena@example.com>
2025-03-31 00:17:53 +0200

    Added agent auth middelware to the agent scan result collector endpoint

--- routes/scan.js
+++ routes/scan.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const router = express.Router();
+const authenticateAgent = require('../middelwares/agentAuthMiddleware');
+const authMiddleware = require('../middelwares/authMiddleware');
+const roleMiddleware = require('../middelwares/roleMiddelware');
 const scanResultController = require('../controllers/scanController');
 
-router.post('/', scanResultController.createScanResult);
+const router = express.Router();
+
+
+router.post('/', authenticateAgent, scanResultController.createScanResult);
 
 router.get('/agent/:agent_id', scanResultController.getScanResultsByAgent);
 
