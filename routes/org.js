b488bb40db079164cf9e223b351dc0c904e6493e
serena <serena@example.com>
2025-05-19 17:27:10 +0200

    Update Agents auth and authorization

--- routes/org.js
+++ routes/org.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const organizationController = require('../controllers/orgController');
+const roleMiddleware = require('../middelwares/roleMiddelware');
+const authMiddleware = require('../middelwares/authMiddleware');
 
 
-router.get('/all', organizationController.getAllOrganizations);
-router.post('/', organizationController.createOrganization);
-router.get('/:org_id', organizationController.getOrganizationById);
-router.put('/:org_id', organizationController.updateOrganization);
-router.delete('/:org_id', organizationController.deleteOrganization);
-router.post('/:org_id/agent/:agent_id', organizationController.addAgentToOrganization);
-router.delete('/:org_id/agent/:agent_id', organizationController.removeAgentFromOrganization);
-router.delete('/:org_id/agent', organizationController.deleteAllAgentsFromOrganization);
+router.get('/all', authMiddleware, roleMiddleware(['root']), organizationController.getAllOrganizations);
+router.post('/', authMiddleware, roleMiddleware(['root']), organizationController.createOrganization);
+router.get('/:org_id', authMiddleware, roleMiddleware(['root', 'integrator', 'admin']), organizationController.getOrganizationById);
+router.put('/:org_id',  authMiddleware, roleMiddleware(['root', 'integrator', 'admin']), organizationController.updateOrganization);
+router.delete('/:org_id',  authMiddleware, roleMiddleware(['root']), organizationController.deleteOrganization);
+router.post('/:org_id/agent/:agent_id',  authMiddleware, roleMiddleware(['root', 'integrator']), organizationController.addAgentToOrganization);
+router.delete('/:org_id/agent/:agent_id',  authMiddleware, roleMiddleware(['root', 'integrator']), organizationController.removeAgentFromOrganization);
+router.delete('/:org_id/agent',  authMiddleware, roleMiddleware(['root', 'integrator']), organizationController.deleteAllAgentsFromOrganization);
 
 module.exports = router;
